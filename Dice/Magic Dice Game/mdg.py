6bd4cafe7300963efe5b5085f3422fbc6ef2b136
gntsqid <132708602+gntsqid@users.noreply.github.com>
2023-06-20 16:48:20 -0700

    Update mdg.py

--- Dice/Magic Dice Game/mdg.py
+++ Dice/Magic Dice Game/mdg.py
@@ -1,22 +1,33 @@
 import random
 
-health = 5
+playerHealth = 5
+enemyHealth  = 5
 
 def d6():
   rng = random.SystemRandom()
   return rng.randint(1, 6)
 
-def prompt():
+def promptPlayer():
   word = input("Type roll when you're ready to go: ")
   if word == "roll":
     return d6()
   else:
     print("That wasn't quite right...try again..\n")
     word = prompt()
-def check(num):
+
+def checkRoll(num):
+  print("\nnum was rolled")
   flag = False
   print("flag is set from True to " + str(flag) + '!')
   return flag
+
+def checkHealth():
+  if playerHealth <= 0:
+    return False
+  if  enemyHealth <= 0:
+    winner = intro().name
+    return False
+    
   
 def intro():
   name = input("Ready to play? Tell me your name: ")
@@ -28,11 +39,20 @@ def intro():
 
 #plays the game
 def init():
-  flag = True
-  name = intro()
+  flag   = True
+  winner = ""
+  name   = intro()
   while flag == True:
-    num = prompt()
-    flag = check(num)
+    winner  = promptPlayer()
+    checkRoll(num)
+    flag = checkHealthP()
+    num  = promptEnemy()
+    checkRoll(num)
+    flag = checkHealth()
   print("Game Over")
+  
 
 init()
+# something is wrong here...need to fix it so it checks both player and enemy health,
+# determines the winner if there is one,
+# then ends the game if there is
