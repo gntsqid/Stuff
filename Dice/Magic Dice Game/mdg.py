b42b87f424e477cfc9dcc3130791ef2e738b3f94
gntsqid <132708602+gntsqid@users.noreply.github.com>
2023-06-20 16:23:45 -0700

    Update mdg.py

--- Dice/Magic Dice Game/mdg.py
+++ Dice/Magic Dice Game/mdg.py
@@ -1,19 +1,27 @@
 import random
 
-health = 10
+health = 5
 
 def d6():
   rng = random.SystemRandom()
   return rng.randint(1, 6)
 
-def prompt(flag):
-  print("flag is set from " + str(flag) + " to false!")
-  return False
-
+def prompt():
+  word = input("Type roll when you're ready to go: ")
+  if word == "roll":
+    return d6()
+  else:
+    word = input("That wasn't right...try again: ")
+def check(num):
+  flag = False
+  print("flag is set from True to + flag + "!")
+  return flag
+  
 def intro():
   name = input("Ready to play? Tell me your name: ")
   print("Hi " + name + "! Let's get started.")
-  print("We both start with 5 lives, roll the dice to lose, gain, or inflict damage on me."
+  print("We both start with " + health + " lives, roll the dice to lose, gain, or inflict damage on me.")
+  print("Last one standing wins...")
   print("Good Luck!\n")
   return name
 
@@ -22,7 +30,8 @@ def init():
   flag = True
   name = intro()
   while flag == True:
-    flag = prompt()
+    num = prompt()
+    flag = check(num)
   print("Game Over")
 
 init()
