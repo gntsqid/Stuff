78e9d98e244bf58758359b4ea6180d392b9993ba
gntsqid <132708602+gntsqid@users.noreply.github.com>
2023-06-21 14:16:00 -0700

    Re-worked everything -needs finishing

--- Dice/Magic Dice Game/mdg.py
+++ Dice/Magic Dice Game/mdg.py
@@ -1,38 +1,60 @@
 import random
 
+global playerHealth 
+global enemyHealth 
+global winner
 playerHealth = 5
 enemyHealth  = 5
+winner = ""
 
 def d6():
   rng = random.SystemRandom()
   return rng.randint(1, 6)
 
-def promptPlayer():
-  word = input("Type roll when you're ready to go: ")
-  if word == "roll":
-    return d6()
-  else:
-    print("That wasn't quite right...try again..\n")
-    word = prompt()
-
-def checkRoll(num):
-  print("\nnum was rolled")
-  flag = False
-  print("flag is set from True to " + str(flag) + '!')
-  return flag
-
 def checkHealth():
   if playerHealth <= 0:
+    winner = "Computer"
     return False
-  if  enemyHealth <= 0:
-    winner = intro().name
-    return False
-    
-  
+  else: 
+    return True
+
+def checkRoll(roll):
+  match roll:
+    case 1:
+      playerHealth -= 1
+      print("That looks like it's going to leave a mark!")
+    case 2:
+      print("testing 2")
+    case 3:
+      print("Wild card time baby!\n Let's gooooo!")
+      checkRoll(d6())
+      checkRoll(d6())
+      checkRoll(d6())
+    case 4:
+      print("Sorry bud looks like nothing happened..")
+    case 5:
+      playerHealth += 1
+      print("You gained a life! How's that fair?!")
+    case 6:
+      enemyHealth -= 1
+      print("Agh I've been wounded!")
+
+def playerTurn():
+  word = input("Type roll when you're ready to go: ")
+  roll = d6()
+  if word == "roll":
+    print("You rolled " + str(roll) + "!")
+    checkRoll(roll)
+    result = checkHealth()
+    return result
+  else:
+    print("That wasn't quite right...try again..\n")
+    word = playerTurn()
+
 def intro():
   name = input("Ready to play? Tell me your name: ")
   print("Hi " + name + "! Let's get started.")
-  print("We both start with " + str(health) + " lives, roll the dice to lose, gain, or inflict damage on me.")
+  print("We both start with " + str(playerHealth) + " lives, roll the dice to lose, gain, or inflict damage on me.")
   print("Last one standing wins...")
   print("Good Luck!\n")
   return name
@@ -40,15 +62,10 @@ def intro():
 #plays the game
 def init():
   flag   = True
-  winner = ""
   name   = intro()
   while flag == True:
-    winner  = promptPlayer()
-    checkRoll(num)
-    flag = checkHealthP()
-    num  = promptEnemy()
-    checkRoll(num)
-    flag = checkHealth()
+    flag = playerTurn()
+  print(winner + " won!")
   print("Game Over")
   
 
