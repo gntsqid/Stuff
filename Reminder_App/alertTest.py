3b575dd3921fc33fc987b0b26fe14554bd32e781
gntsqid <sjlang.805@gmail.com>
2023-07-03 23:29:30 +0000

    Added alert python script

---
+++ Reminder_App/alertTest.py
@@ -0,0 +1,17 @@
+import json
+
+def read_tasks():
+    with open('tasks.json', 'r') as f:
+        tasks = json.load(f)
+    return tasks
+
+def generate_message(tasks):
+    message = "Hi! Remember that you have to do:\n"
+    for task in tasks:
+        message += f"{task['task']} at {task['time']}\n"
+    return message
+
+tasks = read_tasks()
+message = generate_message(tasks)
+print(message)
+
