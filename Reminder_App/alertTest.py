301e668092599d8131b8cd346295e19b0a2902f9
gntsqid <132708602+gntsqid@users.noreply.github.com>
2023-07-04 09:07:09 -0700

    Added file path for my Windows PC

--- Reminder_App/alertTest.py
+++ Reminder_App/alertTest.py
@@ -1,6 +1,7 @@
 import json
 
 def read_tasks():
+    # for my windows desktop: C:\Users\Steven\Desktop\Python\tasks.json
     with open('tasks.json', 'r') as f:
         tasks = json.load(f)
     return tasks
