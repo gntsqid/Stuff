c7aacd116d80d27094b8c0d8b64bd1a57c215b2f
gntsqid <132708602+gntsqid@users.noreply.github.com>
2023-07-18 11:34:18 -0700

    added comment REM

--- wsl.bat
+++ wsl.bat
@@ -1,2 +1,4 @@
 @echo off
+REM hack to wait 17 seconds before attempting to connect by doing empty pings to self
+ping localhost -n 18 > nul 
 powershell -NoExit -Command "& {cd 'G:\Scripts'; echo "Hello"; wsl.exe --set-default docker-desktop; wsl 'ls'; wsl ash 'test.sh'; wsl}"
