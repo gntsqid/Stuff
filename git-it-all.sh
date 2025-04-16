0db9b18154f1705ba12ef1ae05d5ab8e56e9b1a0
gntsqid <132708602+gntsqid@users.noreply.github.com>
2025-04-16 07:31:35 -0700

    Create git-it-all.sh

---
+++ git-it-all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# NOTE: Needs to be a cron job
+
+# Start from the home directory
+start_directory="$HOME"
+
+# Find all .git directories recursively
+find "$start_directory" -type d -name ".git" | while read -r gitdir; do
+    # Move to the parent directory of each .git folder
+    repo_dir=$(dirname "$gitdir")
+    #echo "Entering repository: $repo_dir"
+    cd "$repo_dir" || continue
+
+    # Execute git pull
+    #echo "Running 'git pull' in $repo_dir"
+    git pull 2>/dev/null
+
+    # Navigate back to the start directory
+    cd "$start_directory" || exit
+done
